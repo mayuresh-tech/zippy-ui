83162e90d35420223a9a2ce51a0419ac9e0bebad
mayuresh-tech <mmayuresh12@gmail.com>
2022-02-13 19:07:12 +0530

    feat: form done

--- documentation/docs-js/index.js
+++ documentation/docs-js/index.js
@@ -6,6 +6,51 @@ var dropDownList = document.getElementById("dropdown-list");
 var scrollToTopBtn = document.getElementById("scroll-btn");
 var closeBtnAlert = document.getElementById("close-btn");
 var closeAlert = document.getElementById("close-alert");
+var inputEmail = document.getElementById("email-valid");
+var inputPassword = document.getElementById("password-valid");
+var inputCheckBox = document.getElementById("checkbox-valid");
+const errorMsg = document.getElementsByClassName("error-msg");
+var submitBtn = document.getElementById("submit-btn");
+
+errorMsg[0].style.display = "none";
+errorMsg[1].style.display = "none";
+errorMsg[2].style.display = "none";
+
+submitBtn.addEventListener("click", () => {
+  checkValidEmail();
+  checkValidPassword();
+  checkCheckBox();
+});
+
+function checkCheckBox() {
+  if (!inputCheckBox.checked) {
+    errorMsg[2].style.display = "block";
+  }
+  else {
+    errorMsg[2].style.display = "none";
+  }
+}
+
+function checkValidPassword() {
+  if (inputPassword.value.length < 8) {
+    errorMsg[1].style.display = "block";
+  }
+  else {
+    errorMsg[1].style.display = "none";
+  }
+}
+
+function checkValidEmail() {
+  var emailPattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+  console.log(inputEmail.value);
+  console.log(emailPattern.test(inputEmail.value));
+  if (!emailPattern.test(inputEmail.value)) {
+    errorMsg[0].style.display = "block";
+  }
+  else {
+    errorMsg[0].style.display = "none";
+  }
+}
 
 let dropdownListExpanded = false;
 
